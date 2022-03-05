3c962717e8d605ca9b45c1c6ad93c96ddc7c28ec
PD1411coder <desaiprathamesh1411@gmail.com>
2022-03-05 11:21:06 +0530

    Assignment-4 completed

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,11 @@
 function CreateObject(arr) {
     // Write your code here
+    infoObject = {}
+    len = arr.length
+    for(let i = 0; i < len; i += 2){
+        infoObject[arr[i]] = arr[i+1]
+    }
+    return infoObject
 }
 
 module.exports = CreateObject;
